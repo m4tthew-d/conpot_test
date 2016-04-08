120233cd5a19099005656745df3fbdd583f89535
m4tthew-d <mdavis7@binghamton.edu>
2016-04-08 10:31:36 -0400

    Update stdin_recorder.py
    I updated the style of reading from stdin and fixed the indentations.

--- stdin_recorder.py
+++ stdin_recorder.py
@@ -1,15 +1,14 @@
 # Matthew Davis
 # Task: Create script that reads from stdin and writes to a file
 import sys
-log = open('stdin_log.txt','w')		# Creates file for writing
-while 1:
-	try:
-		line = sys.stdin.readline()	# Reads stdin
-	except KeyboardInterrupt:		# Stops reading when Ctrl-C is pressed
-		break
 
-	if not line:					# Stops reading if line is empty
-		break
-
-	log.write(line)					# writes stdin line to the file
-log.close()
+with open('stdin_log.txt', 'w') as log:
+    while 1:
+        try:
+            line = sys.stdin.read(1048576)  # Reads stdin up to 1 MB
+            if not line:  # Stops reading if line is empty
+                break
+            log.write(line)  # writes stdin line to the file
+            log.flush()
+        except KeyboardInterrupt:  # Stops reading when Ctrl-C is pressed
+            break
